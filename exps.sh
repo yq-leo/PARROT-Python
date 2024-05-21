b7883e2b4f93c49ab488072490bc20529d5f3597
qiyu6 <qiyu6@illinois.edu>
2024-05-21 17:37:16 -0500

    create exp bash

---
+++ exps.sh
@@ -0,0 +1,29 @@
+python main.py --dataset=ACM-DBLP-A
+python main.py --dataset=ACM-DBLP-A
+python main.py --dataset=ACM-DBLP-A
+python main.py --dataset=ACM-DBLP-A
+python main.py --dataset=ACM-DBLP-A
+
+python main.py --dataset=ACM-DBLP-P
+python main.py --dataset=ACM-DBLP-P
+python main.py --dataset=ACM-DBLP-P
+python main.py --dataset=ACM-DBLP-P
+python main.py --dataset=ACM-DBLP-P
+
+python main.py --dataset=cora
+python main.py --dataset=cora
+python main.py --dataset=cora
+python main.py --dataset=cora
+python main.py --dataset=cora
+
+python main.py --dataset=foursquare-twitter
+python main.py --dataset=foursquare-twitter
+python main.py --dataset=foursquare-twitter
+python main.py --dataset=foursquare-twitter
+python main.py --dataset=foursquare-twitter
+
+python main.py --dataset=phone-email
+python main.py --dataset=phone-email
+python main.py --dataset=phone-email
+python main.py --dataset=phone-email
+python main.py --dataset=phone-email
