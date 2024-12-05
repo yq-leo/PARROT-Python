26d2830d1001dce3e8e19ff530b1e4d3eed0c669
qiyu6 <qiyu6@illinois.edu>
2024-12-04 19:55:46 -0600

    update

---
+++ exps/scale.sh
@@ -0,0 +1,6 @@
+python main.py --dataset=phone-email --runs=3 --record
+python main.py --dataset=foursquare-twitter --runs=3 --record
+python main.py --dataset=ACM-DBLP-P --runs=3 --record
+python main.py --dataset=cora --runs=3 --record
+python main.py --dataset=ACM-DBLP-A --runs=3 --record
+python main.py --dataset=Douban --runs=3 --record
